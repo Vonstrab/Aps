8d6bfe5f6c86d88968501e19fc41b3f36b99bbb6
Vonstrab <delgado.ivan@protonmail.com>
2020-03-16 10:17:13 +0100

    more type check

--- src/rum_type.rs
+++ src/rum_type.rs
@@ -7,7 +7,30 @@ pub enum Type {
     Void,
     Func(Vec<Type>, Box<Type>),
     Vector(Box<Type>),
-    TypeError(String)
+    TypeError(String),
+}
+
+impl Type {
+    pub fn is_err(&self) -> bool {
+        match self {
+            Type::TypeError(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_ok(&self) -> bool {
+        match self {
+            Type::TypeError(_) => false,
+            _ => true,
+        }
+    }
+
+    pub fn err_message(&self) -> String {
+        match self {
+            Type::TypeError(err_msg) => err_msg.clone(),
+            _ => panic!("the type is not a type error"),
+        }
+    }
 }
 
 impl std::fmt::Debug for Type {
@@ -15,7 +38,7 @@ impl std::fmt::Debug for Type {
         use self::Type::*;
         match self {
             Int => write!(fmt, "int"),
-            Void => write!(fmt, "int"),//TODO Check if its right
+            Void => write!(fmt, "int"), //TODO Check if its right
             Bool => write!(fmt, "bool"),
 
             Func(args, retour) => {
@@ -26,9 +49,9 @@ impl std::fmt::Debug for Type {
                     }
                 }
                 write!(fmt, " -> {:?}", retour)
-            },
+            }
             Vector(t) => write!(fmt, "vec( {:?} )", t),
-            TypeError (error_text) => write!(fmt, "Type Error( {:?} )", error_text)
+            TypeError(error_text) => write!(fmt, "Type Error( {:?} )", error_text),
         }
     }
 }
@@ -38,7 +61,7 @@ impl std::fmt::Display for Type {
         use self::Type::*;
         match self {
             Int => write!(fmt, "int"),
-            Void => write!(fmt, "int"),//TODO Check if its right
+            Void => write!(fmt, "int"), //TODO Check if its right
             Bool => write!(fmt, "bool"),
 
             Func(args, retour) => {
@@ -49,10 +72,9 @@ impl std::fmt::Display for Type {
                     }
                 }
                 write!(fmt, " -> {}", retour)
-            },
+            }
             Vector(t) => write!(fmt, "vec( {} )", t),
-            TypeError (error_text) => write!(fmt, "Type Error( {} )", error_text)
-
+            TypeError(error_text) => write!(fmt, "Type Error( {} )", error_text),
         }
     }
 }
--- src/type_checker.rs
+++ src/type_checker.rs
@@ -6,116 +6,153 @@ use crate::rum_type::Type;
 
 use std::collections::HashMap;
 
-// impl AstExp {
-//     pub fn type_check(&self, type_cache: &HashMap<String, Type>) -> Type {
-//         println!("\nInto Expr eval");
-//         println!("expr : {:?}", self);
-//         println!("type_cache {:?}", type_cache);
+impl AstExp {
+    pub fn type_check(&self, type_cache: &HashMap<String, Type>) -> Type {
+        println!("\nInto Expr eval");
+        println!("expr : {:?}", self);
+        println!("type_cache {:?}", type_cache);
 
-//         use AstExp::*;
+        use AstExp::*;
 
-//         match self {
-//             ASTInt(n) => Type::Int,
-//             ASTBool(b) => Type::Bool,
-//             ASTUnPrim(_, exp) => {
-//                 let exp_type = exp.type_check(type_cache);
-//                 if exp_type == Type::Bool {
-//                     Type::Bool
-//                 } else {
-//                     Type::TypeError(format!("Not primitive function require a boolean argument"))
-//                 }
-//             }
-//             ASTBinPrim(op, e1, e2) => {
-//                 let expr1 = e1.type_check(type_cache);
-//                 let expr2 = e2.type_check(type_cache);
+        match self {
+            ASTInt(n) => Type::Int,
+            ASTBool(b) => Type::Bool,
+            ASTUnPrim(_, exp) => {
+                let exp_type = exp.type_check(type_cache);
+                if exp_type == Type::Bool {
+                    Type::Bool
+                } else {
+                    Type::TypeError(format!("Not primitive function require a boolean argument"))
+                }
+            }
+            ASTBinPrim(op, e1, e2) => {
+                let expr1 = e1.type_check(type_cache);
+                let expr2 = e2.type_check(type_cache);
 
-//                 match op {
-//                     Oprim::Add => Value::Int(expr1 + expr2),
-//                     Oprim::Mul => Value::Int(expr1 * expr2),
-//                     Oprim::Div => Value::Int(expr1 / expr2),
-//                     Oprim::Sub => Value::Int(expr1 - expr2),
-//                     Oprim::Eq => Value::Int(Value::to_int(expr1 == expr2)),
-//                     Oprim::Lt => Value::Int(Value::to_int(expr1 < expr2)),
-//                     Oprim::And => Value::Int(Value::to_int(
-//                         Value::to_bool(expr1) && Value::to_bool(expr2),
-//                     )),
-//                     Oprim::Or => Value::Int(Value::to_int(
-//                         Value::to_bool(expr1) || Value::to_bool(expr2),
-//                     )),
-//                 }
-//             }
-//             ASTIf(condition, then, _else) => {
-//                 let type_condition = condition.type_check(type_cache);
-//                 let type_then = then.type_check(type_cache);
-//                 let type_else = _else.type_check(type_cache);
+                if expr1.is_err() || expr2.is_err() {
+                    return Type::TypeError(format!("TODO"));
+                }
 
-//                 if
-//             }
+                match op {
+                    Oprim::Add => {
+                        if expr1 != Type::Int || expr2 != Type::Int {
+                            Type::TypeError(format!("TODO"))
+                        } else {
+                            Type::Int
+                        }
+                    }
+                    Oprim::Mul => {
+                        if expr1 != Type::Int || expr2 != Type::Int {
+                            Type::TypeError(format!("TODO"))
+                        } else {
+                            Type::Int
+                        }
+                    }
+                    Oprim::Div => {
+                        if expr1 != Type::Int || expr2 != Type::Int {
+                            Type::TypeError(format!("TODO"))
+                        } else {
+                            Type::Int
+                        }
+                    }
+                    Oprim::Sub => {
+                        if expr1 != Type::Int || expr2 != Type::Int {
+                            Type::TypeError(format!("TODO"))
+                        } else {
+                            Type::Int
+                        }
+                    }
+                    Oprim::Eq => {
+                        if expr1 != Type::Bool || expr2 != Type::Bool {
+                            Type::TypeError(format!("TODO"))
+                        } else {
+                            Type::Int
+                        }
+                    }
+                    Oprim::Lt => {
+                        if expr1 != Type::Bool || expr2 != Type::Bool {
+                            Type::TypeError(format!("TODO"))
+                        } else {
+                            Type::Int
+                        }
+                    }
+                    Oprim::And => {
+                        if expr1 != Type::Bool || expr2 != Type::Bool {
+                            Type::TypeError(format!("TODO"))
+                        } else {
+                            Type::Int
+                        }
+                    }
+                    Oprim::Or => {
+                        if expr1 != Type::Bool || expr2 != Type::Bool {
+                            Type::TypeError(format!("TODO"))
+                        } else {
+                            Type::Int
+                        }
+                    }
+                }
+            }
+            ASTIf(condition, then, _else) => {
+                let type_condition = condition.type_check(type_cache);
+                let type_then = then.type_check(type_cache);
+                let type_else = _else.type_check(type_cache);
 
-//             ASTIdent(x) => {
-//                 let id = env.get(x).expect("Not in environment");
-//                 match id {
-//                     Value::Adress(adr) => mem.mem[*adr].clone(),
-//                     _ => id.clone(),
-//                 }
-//             }
+                if type_condition != Type::Bool {
+                    return Type::TypeError(format!("TODO"));
+                }
 
-//             ASTApp(e, args) => match &env[e] {
-//                 Value::Fermeture(fbody, fargs, fenv) => {
-//                     let mut nfenv = env.clone();
+                if type_then != type_else {
+                    return Type::TypeError(format!("TODO"));
+                }
 
-//                     for (f, value) in fenv {
-//                         nfenv.insert(f.clone(), value.clone());
-//                     }
+                type_then
+            }
 
-//                     for i in 0..fargs.len() {
-//                         nfenv.insert(fargs[i].clone(), args[i].eval(env, mem));
-//                     }
+            ASTIdent(x) => type_cache.get(x).expect("Not in environment").clone(),
 
-//                     fbody.eval(&nfenv, mem)
-//                 }
+            ASTApp(e, args) => {
+                let func_type = type_cache.get(e).expect("Not in environment").clone();
+                let type_args = Vec::with_capacity(args.len());
 
-//                 Value::FermetureRec(fbody, fargs, fenv) => {
-//                     let mut nfenv = fenv.clone();
-//                     for i in 0..fargs.len() {
-//                         nfenv.insert(fargs[i].clone(), args[i].eval(env, mem));
-//                     }
-//                     fbody.eval(&nfenv, mem)
-//                 }
+                for arg in args {
+                    type_args.push(arg.type_check(type_cache));
+                }
 
-//                 _ => panic!("not a fermeture"),
-//             },
+                Type::check_fun(func_type, type_args)
+            }
 
-//             ASTAbs(args, e) => {
-//                 let mut abs_args = Vec::new();
+            ASTAbs(args, e) => {
+                let mut abs_args = Vec::new();
 
-//                 for arg in args {
-//                     abs_args.push(arg.ident.clone());
-//                 }
-//                 Value::Fermeture(e.clone(), abs_args, env.clone())
-//             }
+                for arg in args {
+                    abs_args.push(arg.ident.clone());
+                }
+                Value::Fermeture(e.clone(), abs_args, env.clone())
+            }
 
-//             // rum2
-//             ASTAlloc(e) => match e.eval(&env, mem) {
-//                 Value::Int(n) => mem.allocn(n as usize),
-//                 _ => panic!(format!(" {:?} is not a Number", e)),
-//             },
-//             ASTNth(e1, e2) => match e1.eval(&env, mem) {
-//                 Value::Block(adr, n) => match e2.eval(&env, mem) {
-//                     Value::Int(i) => {
-//                         if i > n as i64 {
-//                             panic!("ASTNTH out of the block");
-//                         }
-//                         mem.mem[adr + i as usize].clone()
-//                     }
-//                     _ => panic!(format!(" {:?} is not a Number", e2)),
-//                 },
-//                 _ => panic!(format!(" {:?} is not a Block", e1)),
-//             },
-//             ASTLen(e) => match e.eval(&env, mem) {
-//                 Value::Block(_, n) => Value::Int(n as i64),
-//                 _ => panic!(format!(" {:?} is not a Block", e)),
-//             },
-//         }
-//     }
-// }
+            // rum2
+            ASTAlloc(e) => match e.eval(&env, mem) {
+                Value::Int(n) => mem.allocn(n as usize),
+                _ => panic!(format!(" {:?} is not a Number", e)),
+            },
+
+            ASTNth(e1, e2) => match e1.eval(&env, mem) {
+                Value::Block(adr, n) => match e2.eval(&env, mem) {
+                    Value::Int(i) => {
+                        if i > n as i64 {
+                            panic!("ASTNTH out of the block");
+                        }
+                        mem.mem[adr + i as usize].clone()
+                    }
+                    _ => panic!(format!(" {:?} is not a Number", e2)),
+                },
+                _ => panic!(format!(" {:?} is not a Block", e1)),
+            },
+            
+            ASTLen(e) => match e.eval(&env, mem) {
+                Value::Block(_, n) => Value::Int(n as i64),
+                _ => panic!(format!(" {:?} is not a Block", e)),
+            },
+        }
+    }
+}
